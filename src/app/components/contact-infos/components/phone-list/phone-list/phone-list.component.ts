c528b399ee0f7fd84e98c56e22727db0e4d60312
Lucas <lpimentel32@hotmail.com>
2026-04-05 19:03:55 -0300

    feat: adicionar componente de listagem de telefones do usuário

---
+++ src/app/components/contact-infos/components/phone-list/phone-list/phone-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { PhoneTypeEnum } from 'src/app/enums/phone-type.enum';
+import { IPhoneToDisplay } from 'src/app/interfaces/user/phone-to-display';
+import { IPhone } from 'src/app/interfaces/user/phone.interface';
+import { phoneTypeDescriptionMap } from 'src/app/utils/phone-type-description-map';
+import { preparePhoneList } from 'src/app/utils/prepare-phone-list';
+
+@Component({
+  selector: 'app-phone-list',
+  templateUrl: './phone-list.component.html',
+  styleUrls: ['./phone-list.component.scss'],
+})
+export class PhoneListComponent implements OnChanges {
+  @Input() phones: IPhone[] | undefined = [];
+  phoneListToDisplay: IPhoneToDisplay[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (Array.isArray(changes['phones'].currentValue)) {
+      this.preparePhoneListToDisplay();
+    }
+  }
+
+  preparePhoneListToDisplay() {
+    this.phoneListToDisplay = [];
+
+    const originalUserPhoneList =
+      this.phones && this.phones.length > 0 ? this.phones : [];
+
+    preparePhoneList(originalUserPhoneList, true, (phone) => {
+      this.phoneListToDisplay.push(phone);
+    });
+  }
+}
